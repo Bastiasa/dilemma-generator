caea7061584f56c1a4673f467aaa55ff8869e277
Bastiasa <leysantqm@gmail.com>
2025-10-20 19:20:52 -0500

    mvp2

--- api/generate.ts
+++ api/generate.ts
@@ -15,21 +15,43 @@ const ai = new GoogleGenAI({
 
 async function generateDilemmas(): Promise<string[]> {
 
-    const prompt = `Genera diez dilemas morales o éticos en español. 
-Deben ser preguntas de sí o no, sin explicación adicional.
-No uses temas repetidos ni clichés.
-Sé bastante creativo, intenta crear una pregunta que no sea muy común y que deje a los lectores introspeccionando.
-Debe ser un array que contenga unicamente strings. SOLO EL TEXTO DEL ARRAY, SIN NADA QUE PUEDA DIFICULTAR LA LECTURA POR PARTE DE JSON.
-No pueden ser dilemas muy complejos o con un vocabulario muy rebuscado, pues es dirigido a jóvenes entre 8 a 14 años.`;
+    const prompt = `Genera diez dilemas morales o éticos en español, dirigidos a un público joven (de 15 a 25 años).
+Requisitos:
+- Cada dilema debe formularse como una sola pregunta de "sí o no"
+- Deben ser dilemas difíciles de responder, donde ambas opciones parezcan moralmente incorrectas o conflictivas
+- No hagas preguntas que sean muy generales, haz preguntas que sean MUY PERSONALES, que jueguen bastante con el lector
+- Juega bastante con la percepción de bondad y maldad de los lectores
+- No incluyas explicaciones, contexto ni numeración
+- Se demasiado claro y directo, que pueda entenderse fácilmente
+- Devuelve ÚNICAMENTE un array JSON con 10 strings o con un array de respuesta múltiple. No escribas texto fuera del array ni comentarios
+
+- IMPORTANTE: También sé muy personal con el tema familiar, de forma en la que hagas preguntas sobre "perdonar traumas".
+- IMPORTANTE: Haz preguntas que afecten mucho al lector o a sus conocidos, no incluyas a terceros por los que el lector pueda sentir indiferencia
+
+
+Ejemplo de formato de salida esperado
+
+[
+  "¿Salvarías la vida de alguien que te cae mal?",
+  "¿Perdonarías a tu hermano si te quitara a tu novia?",
+  "¿Estarías con alguien que no amas, solo para complacer a tu familia?"
+]`;
 
     const geminiResponse = await ai.models.generateContent({
         config: {
-            temperature: 1,
-            topP: 0.5,
-            topK: 125
+            temperature: 0.9,
+            topP: 0.8,
+            topK: 60
         },
-        model: 'gemini-2.0-flash-001',
-        contents: prompt
+        model: 'gemini-2.0-flash-lite',
+        contents: [
+            {
+                role: 'user',
+                parts: [
+                    { text: prompt }
+                ]
+            }
+        ]
     });
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,172 @@
 import { useEffect, useRef, useState } from 'react';
+import { motion } from "framer-motion";
+import SplitText from './components/SplitText';
+
 import './App.css';
 
 
-
+type CurrentState = 'loading' | 'question' | 'percentage';
+type Answers = Record<string, { yes: number; no: number; }>;
 
 function App() {
-  const [text, setText] = useState('LOADING...');
-  const dilemmasRef = useRef<string[]>([]);
+  const [question, setQuestion] = useState('');
+  const [currentMode, setCurrentMode] = useState<CurrentState>('loading');
 
-  function onGenerateDilemmaClicked() {
+  const percentageValuesRef = useRef<[number, number]>([NaN, NaN]);
+
+  const loadingDilemmas = useRef<Promise<any>>(undefined);
+
+  const answersRef = useRef<Answers>(JSON.parse(localStorage.getItem('answers') as string) ?? {});
+
+  const dilemmasRef = useRef<string[]>([]);
+  const questionIdRef = useRef(0);
+  const questionCountRef = useRef(0);
+
+  async function loadDilemmas() {
+    const response = await fetch('/api/generate/');
+    const generatedDilemmas = await response.json() as string[];
+    dilemmasRef.current = [...dilemmasRef.current, ...generatedDilemmas];
+  }
+
+  function checkDilemmas() {
+    if (dilemmasRef.current.length < 10) {
+      loadingDilemmas.current = new Promise<void>(async (resolve) => {
+        await loadDilemmas();
+        await loadDilemmas();
+        console.log("Dilemmas reloaded");
+        
+        resolve();
+      });
+    }
+  }
+
+  function showNextDilemma() {
 
     const dilemmas = dilemmasRef.current;
 
+    checkDilemmas();
+
     if (dilemmas.length > 0) {
-      setText(dilemmas.shift() as string);
-      console.log(dilemmas);
-      
+      questionIdRef.current++;
+      questionCountRef.current++;
+      setQuestion(dilemmas.shift() as string);
       return;
     }
 
-    setText('CARGANDO...');
+    setCurrentMode('loading');
 
-    (async () => {
-
-      const response = await fetch('/api/generate/');
-      const generatedDilemmas = await response.json();
-
-
-      console.log('Generated dilemmas: ', generatedDilemmas);
-      
-
-
-      dilemmasRef.current = generatedDilemmas;
-      setText(dilemmasRef.current.shift() as string);
-      console.log(dilemmas);
-     })();
+    loadingDilemmas.current?.then(() => {
+      showNextDilemma();
+    });
   }
 
-  useEffect(() => onGenerateDilemmaClicked(), []);
+  function optionClicked(option: 'yes' | 'no') {
+
+    if (questionCountRef.current >= 2) {
+      questionCountRef.current = 0;
+      const answeredQuestions = Object.keys(answersRef.current);
+      dilemmasRef.current.unshift(answeredQuestions[Math.floor(answeredQuestions.length - 1 * Math.random())]);
+      
+    }
+    
+    if (answersRef.current[question] == undefined) {
+      answersRef.current[question] = {
+        yes: 0,
+        no: 0
+      }
+    } else {
+      answersRef.current[question][option]++;
+      const { yes, no } = answersRef.current[question];
+
+      const total = yes + no;
+
+      const yesPercentage = yes / total * 100;
+      const noPercentage = no / total * 100;
+
+      percentageValuesRef.current = [yesPercentage, noPercentage];
+      setCurrentMode('percentage');
+      setTimeout(() => {
+        showNextDilemma();
+        setCurrentMode('question');
+      }, 3000);
+
+      return;
+    }
+
+
+    answersRef.current[question][option]++;
+    showNextDilemma();
+    localStorage.setItem('answers', JSON.stringify(answersRef.current));
+  }
+
+  useEffect(() => {
+    (async () => {
+
+      showNextDilemma();
+      await loadingDilemmas.current;
+      setCurrentMode('question');
+
+
+    })();
+  }, []);
 
   return (
-    <>
-      <p>{text}</p>
-      <button onClick={onGenerateDilemmaClicked}>CREAR DILEMA</button>
-    </>
+    <div
+      className='relative w-dvw px-16 max-w-[800px] mx-auto h-dvh vbox gap-2 justify-center items-center'>
+      <SplitText
+        key={`question#${questionIdRef.current}`}
+        duration={0.5}
+        splitType='words'
+        ease={'power3.out'}
+        text={currentMode === 'question' ? question : 'Cargando'}
+        textAlign='center'
+        className='font-bold text-3xl! mb-10'/>
+
+      {currentMode === 'question' && <>
+      
+      
+        <span className='text-[20px]'>Seleccione una respuesta</span>
+        <div className='text-[20px] hbox max-w-[512px] gap-4 w-full'>
+          <button onClick={()=>optionClicked('yes')} className='grow'>Sí</button>
+          <button onClick={()=>optionClicked('no')} className='grow'>No</button>
+        </div>
+      
+      </>}
+
+
+      <div
+        className={`${currentMode == 'percentage' ? 'opvisible' : 'ophidden'} items-center hbox gap-8 fade-enable absolute inset-0 bg-primary`}>
+        {percentageValuesRef.current.map((value, index) => {
+          return <div
+            className='text-center font-extrabold content-center relative grow h-full max-h-[500px]'
+            key={index + questionIdRef.current}>
+            
+            <div className='text-white! relative z-50 mix-blend-difference text-[28px]'>
+              {index == 0 ? 'SÍ' : 'NO'} <br/>
+              {Math.floor(value)}%
+            </div>
+
+            <motion.div
+              key={`${index}#${questionIdRef.current}`}
+              initial={{
+                opacity: 0,
+                height:0
+              }}
+              animate={{
+                opacity: 1,
+                height: `${value}%`
+              }}
+
+              transition={{
+                duration:1.5
+              }}
+              className='bg-black absolute bottom-0 left-0 w-full'>
+              
+              </motion.div>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 

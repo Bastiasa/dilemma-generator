8c59bf355c367ccb4bfd604c4069ddf27bbbda98
Bastiasa <leysantqm@gmail.com>
2025-10-20 20:14:24 -0500

    solo preguntas cerradas

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: "https://bastiasa.github.io/dilemma-generator/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs'
+  }
 })
